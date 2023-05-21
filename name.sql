890d0b8c6eb3099eb61254b984bc1fb02968c871
Christopher Elliot <chriselli.dev@gmail.com>
2023-05-21 22:58:37 +0300

    Lab-3 Database

--- name.sql
+++ name.sql
@@ -6,3 +6,4 @@ CREATE TABLE student_group
 
 CREATE TABLE student
 (student_id integer PRIMARY KEY, full_name text NOT NULL, passport char(10) NOT NULL, fk_numb_id integer REFERENCES student_group (numb_id) NOT NULL);
+#
